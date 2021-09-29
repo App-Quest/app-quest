3a722770453ca86ab35f712b91e408d2bcc3c1db
Jared Lewis <jareddlewis@gmail.com>
2021-09-29 16:19:22 -0500

    got one enzyme test to work, although the server does not close

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// import toJson from 'enzyme-to-json';
+import { Provider } from 'react-redux';
+import store from '../client/store';
 
 // Enzyme is a wrapper around React test utilities which makes it easier to
 // shallow render and traverse the shallow rendered tree.
 import AuthForm from '../client/components/AuthForm';
+import { expect } from '@jest/globals';
 
 // Newer Enzyme versions require an adapter to a particular version of React
 configure({ adapter: new Adapter() });
 
-describe('React unit tests', () => {});
+describe('React unit tests', () => {
+  describe('AuthForm', () => {
+    let wrapper;
+    let email = '';
+    const props = {
+      buttonLabel: '',
+      emailInput: '',
+      passwordInput: '',
+      inputEmail: () => {},
+      inputPassword: () => {},
+      url: 'signin',
+      setApplicationPosts: () => {},
+      signInResponse: () => {},
+      setSignInResponse: () => {},
+    };
+    describe("AuthForm should render all child components in a div with class 'form'", () => {
+      beforeAll(() => {
+        wrapper = shallow(
+          <Provider store={store}>
+            <AuthForm {...props} />
+          </Provider>
+        );
+      });
+
+      it('should render am AuthForm component', () => {
+        console.log('wrapper DEBUG: ', wrapper.debug());
+        expect(wrapper.find(AuthForm)).toHaveLength(1);
+      });
+    });
+  });
+});
